56e3803269fdae6d4a79033e6e2ba0ebdb837e53
khizar <khizarwahab19@gmail.com>
2024-01-09 18:40:20 +0500

    Add title

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     @yield('meta-tags')
     <title>
-        @stack('title')
+       24 Flix @stack('title')
     </title>
     @stack('style')
     {{-- Old Css --}}
