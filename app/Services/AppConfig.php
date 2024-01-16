bcaeff574c1e98026737697d59dbf4a87a0a237c
Sibi haseb <67563045+sibihaseb@users.noreply.github.com>
2024-01-16 21:07:31 +0500

    Added Device detection in App Config

--- app/Services/AppConfig.php
+++ app/Services/AppConfig.php
@@ -23,9 +23,50 @@ class AppConfig
     public static function fetchData()
     {
         if (!Session::get('api_data')) {
+            $finalresultDevice = null;
+            // Get the user agent string
+            $userAgent = $_SERVER['HTTP_USER_AGENT'];
+
+            // Check if the user agent indicates a mobile device
+            $isMobile = (bool)preg_match('/Mobile|Android|iPhone|iPad|iPod|BlackBerry|Windows Phone/i', $userAgent);
+
+            // Check if the user agent indicates a tablet device
+            $isTablet = (bool)preg_match('/iPad|Android|Tablet/i', $userAgent);
+
+            // Initialize a variable to store the matched browser
+            $matchedBrowser = "Unknown";
+
+            // Detect browsers
+            if (strpos($userAgent, 'Chrome') !== false) {
+                $matchedBrowser = "Chrome";
+            } elseif (strpos($userAgent, 'Firefox') !== false) {
+                $matchedBrowser = "Firefox";
+            } elseif (strpos($userAgent, 'Safari') !== false) {
+                $matchedBrowser = "Safari";
+            } elseif (strpos($userAgent, 'Edge') !== false) {
+                $matchedBrowser = "Microsoft Edge";
+            } elseif (strpos($userAgent, 'MSIE') !== false || strpos($userAgent, 'Trident/') !== false) {
+                $matchedBrowser = "Internet Explorer";
+            }
+
+            // Output the information
+            // echo "Is Mobile: " . ($isMobile ? 'Yes' : 'No') . "<br>";
+            // echo "Is Tablet: " . ($isTablet ? 'Yes' : 'No') . "<br>";
+            $mobile = $isMobile ? true : false;
+            $tablet = $isTablet ? true : false;
+            if ($mobile) {
+                $finalresultDevice = 'mobile';
+            } elseif ($isTablet) {
+                $finalresultDevice = 'tablet';
+            } else {
+                $finalresultDevice = $matchedBrowser;
+            }
+
+            $xyz = base64_encode(request()->ip());
             $response = Http::timeout(300)->withHeaders(Api::headers())
-                ->get(Api::endpoint('/masterfeed'));
-                // dd($response);
+                ->get(Api::endpoint("/masterfeed?user_data={$xyz}&user_device={$finalresultDevice}"));
+
+
             Session::put('api_data', $response->body());
         }
     }
