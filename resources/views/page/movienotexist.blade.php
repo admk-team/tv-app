22c75961071c44ae36cf6c16647fc319289d60ed
Khurram <m.khurram4669@gmail.com>
2024-01-17 17:24:03 +0500

    timezones and content not exist

--- resources/views/page/movienotexist.blade.php
+++ resources/views/page/movienotexist.blade.php
@@ -3,8 +3,8 @@
 @section('content')
     <div class="container-fluid" style="margin: 150px 0px 150px 0px;">
         <div class="row">
-            <h1 style="alfont-weight:bold; text-align: center;
-            color: antiquewhite;"> Movie content is not
-                available in your country</h1>
+            <h4 style="alfont-weight:bold; text-align: center;
+            color: antiquewhite;"> Sorry, this content is not
+                available in your country</h4>
         </div>
     @endsection
