cb71581816dc46e8b70f698193369c3f13262f0a
Khurram <m.khurram4669@gmail.com>
2024-02-27 13:09:06 +0500

    watch history resposive done

--- resources/views/watch_history/index.blade.php
+++ resources/views/watch_history/index.blade.php
@@ -21,70 +21,57 @@
         .item {
             padding: 10px;
         }
+
+        .card:hover img {
+            transform: scale(1.1); /* Adjust the scale factor as needed */
+            transition: transform 0.3s ease-in-out;
+        }
     </style>
 @endpush
 
 @section('content')
-    <section class="">
-        <div class="">
-            <div class="listing_box" style="padding-left: 350px">
-                <div class="slider_title_box">
-                    <div class="list_heading">
-                        <h1>Watch History</h1>
+    <section class="container">
+        <div class="row justify-content-center">
+            <div class="col-lg-8 col-md-10">
+                <div class="listing_box">
+                    <div class="slider_title_box">
+                        <div class="list_heading text-center">
+                            <h1>Watch History</h1>
+                        </div>
                     </div>
-                </div>
-                @foreach ($data['app']['streams'] as $key => $stream)
-                    @if ($key >= 30)
-                    @break
-                @endif
-                <div class="d-flex gap-4 mt-4 mb-4" style="max-width: 1000px;">
-                    <div class="">
-                        <div class="ripple">
-                            <a href="{{ route('detailscreen', $stream['stream_guid']) }}">
-                                <div class="thumbnail_img">
-                                    {{--  <div class="trending_icon_box" {!! $stream['stream_guid'] == 'F' ? 'style="display: none;"' : '' !!}>
-                                        <img src="{{ url('/') }}/assets/images/trending_icon.png" alt="Trending">
-                                    </div>  --}}
-                                    <div>
-                                        <img src="{{ $stream['stream_poster'] }}" alt="{{ $stream['stream_title'] }}">
-                                    </div>
-                                    <div class="detail_box_hide">
-                                        <div class="detailbox_time">
-                                            {{ $stream['stream_duration_timeformat'] ?? '' }}
-                                        </div>
-                                        <div class="deta_box">
-                                            <div class="season_title">
+                    @foreach ($data['app']['streams'] as $key => $stream)
+                        @if ($key >= 30)
+                            @break
+                        @endif
+                        <div class="card mb-4 mt-4" style="background-color: black; color:white;">
+                            <div class="row g-0">
+                                <div class="col-md-4">
+                                    <a href="{{ route('detailscreen', $stream['stream_guid']) }}">
+                                        <img src="{{ $stream['stream_poster'] }}" alt="{{ $stream['stream_title'] }}"
+                                            class="img-fluid" style="position: relative;">
+                                        @if ($stream['stream_watched_dur_in_pct'] > 1)
+                                            <div class="progress" style="background-color:#555455;height:5px; border-radius:2px;">
+                                                <div class="progress-bar bg-primary" role="progressbar"
+                                                    style="background-color:#07659E;height:5px;border-radius:2px;width: {{ $stream['stream_watched_dur_in_pct'] }}%"
+                                                    aria-valuenow="{{ $stream['stream_watched_dur_in_pct'] }}"
+                                                    aria-valuemin="0" aria-valuemax="100"></div>
                                             </div>
-                                            <div class="content_title"> {{ $stream['stream_title'] }}</div>
-                                            <div class="content_description">
-                                                {{ $stream['stream_description'] }}
-                                            </div>
-                                            @if ($stream['stream_watched_dur_in_pct'] > 1)
-                                                <div
-                                                    style="background-color:#555455;height:5px; border-radius:2px;margin-top:10px;">
-                                                    <div
-                                                        style="background-color:#07659E;height:5px;border-radius:2px;width:{{ $stream['stream_watched_dur_in_pct'] }}%">
-                                                    </div>
-                                                </div>
-                                            @endif
-                                        </div>
+                                        @endif
+                                    </a>
+                                </div>
+                                <div class="col-md-8 mt-5">
+                                    <div class="card-body">
+                                        <h5 class="card-title">{{ $stream['stream_title'] }}</h5>
+                                        <p class="card-text text-truncate" style="max-height: 8em; overflow: hidden;">
+                                            {{ Illuminate\Support\Str::limit($stream['stream_description'], $limit = 300, $end = '...') }}
+                                        </p>
                                     </div>
                                 </div>
-                            </a>
+                            </div>
                         </div>
-                    </div>
-                    <div class="">
-                        <div class="content_title mt-5" style="font-size: 28px;">{{ $stream['stream_title'] }}
-                        </div>
-                        <div class="content_description mt-5 mb-1"
-                            style=" font-size: 14px;line-height: 22px !important; ">
-                            {{ $stream['stream_description'] }}
-                        </div>
-                    </div>
+                    @endforeach
                 </div>
-            @endforeach
-
+            </div>
         </div>
-    </div>
-</section>
+    </section>
 @endsection
