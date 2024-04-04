16d17e5ef99599134225808b8a2b81a6ac35628d
Khurram <m.khurram4669@gmail.com>
2024-04-04 11:43:15 +0500

    stars iisue resolve for rating

--- resources/views/components/elektra.blade.php
+++ resources/views/components/elektra.blade.php
@@ -28,7 +28,7 @@
         <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 shadow-sm">
             <h5 class="my-0 mr-md-auto font-weight-normal text-white">{{ $data->app->app_info->app_name ?? '' }}</h5>
             <nav class="my-2 my-md-0 mr-md-3">
-                <a class="p-2 text-white" href="javascript:void(0);">Support</a>
+                <a class="p-2 text-white" href="/page/contact-us">Support</a>
             </nav>
             <a class="btn btn-danger" href="/signup">Signup</a>
         </div>
