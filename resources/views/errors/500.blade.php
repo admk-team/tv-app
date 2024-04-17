a617f34ba210337d649fe59fa3f82359883284ba
khizar <khizarwahab19@gmail.com>
2024-04-17 11:31:21 +0500

    Add 500 error page

---
+++ resources/views/errors/500.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.app')
+
+@section('content')
+<section class="sliders topSlider gridSection" style="background: url(https://admk.24flix.tv/images/oops.gif) no-repeat; background-size: cover; background-position:center;height: 100vh; ">
+	<div class="container">
+		<div class="row align-items-center justify-content-center pt-5 hts-100">
+			<div class="error-set">
+			<div class="text-white text-center" style="font-size: 10rem;
+                font-weight: 600; line-height: 1em;">500</div>
+			<div class="text-white text-center fs-2">Oops.. Something went wrong.</div>
+			<div class="text-white text-center">
+				Sorry for the inconvenience, we are working on fixing the problem.
+			</div>
+			</div>
+		</div>
+	</div>
+</section>
+@endsection
