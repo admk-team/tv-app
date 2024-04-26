70383797897bb537ba86bde4b7c96ba5a0aa5933
Khurram <m.khurram4669@gmail.com>
2024-04-26 15:55:08 +0500

    favicon added on iris landing page

--- resources/views/components/iris.blade.php
+++ resources/views/components/iris.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{ $data->app->app_info->app_name ?? '' }}</title>
-
+    <link rel="icon" href="{{ $data->app->app_info->website_faviocn ?? '' }}">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="{{ asset('assets/landing_theme_assets/iris/css/style.css') }}">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" />
