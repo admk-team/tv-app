6dd1b73893105b6c1c3f9c244e30cdaeba894278
Sibi haseb <abdul.haseeb.ali@gmail.com>
2024-05-30 23:56:52 +0500

    Fixed Url of Manage Profile

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -38,9 +38,7 @@ class ProfileController extends Controller
     {
         $response = Http::withHeaders(Api::headers())
             ->asForm()
-            ->get(Api::endpoint('/userprofiles?id=' . session('USER_DETAILS.USER_ID')), [
-                'type' => 'advisory',
-            ]);
+            ->get(Api::endpoint('/userprofiles?id=' . session('USER_DETAILS.USER_ID')));
         $user_data = $response->json();
 
         return view('profile.manage', compact('user_data'));
