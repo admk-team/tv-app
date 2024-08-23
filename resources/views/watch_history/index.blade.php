b9073e3da696a0efeb42282e1ebd9d82cfdafc6e
Khurram <m.khurram4669@gmail.com>
2024-08-23 12:57:56 +0500

    color theme apply on watch history

--- resources/views/watch_history/index.blade.php
+++ resources/views/watch_history/index.blade.php
@@ -1,36 +1,19 @@
 @extends('layouts.app')
 
-@push('style')
-    <style>
-        body {
-            overflow-x: hidden;
-        }
 
-        .main-div {
-            height: 100% !important;
-        }
 
-        .whoIsWatching {
-            padding-top: 120px;
-        }
-
-        .addIcon i {
-            font-size: 7.9vw !important;
-        }
-
-        .item {
-            padding: 10px;
-        }
-
-        .card:hover img {
-            transform: scale(1.1);
-            /* Adjust the scale factor as needed */
-            transition: transform 0.3s ease-in-out;
-        }
-    </style>
-@endpush
 
 @section('content')
+    <style>
+        .card-custom {
+            background-color: var(--bgcolor);
+            color: var(--themePrimaryTxtColor);
+            border: 1px solid var(--themeActiveColor) !important;
+            /* Correct border declaration */
+            border-radius: 4px !important;
+            /* Optional: Adjust as needed */
+        }
+    </style>
     <section class="container">
         <div class="row justify-content-center">
             <div class="col-lg-8 col-md-10">
@@ -45,38 +28,9 @@
                             @if ($key >= 30)
                             @break
                         @endif
-                        <div class="card mb-4 mt-4" style="background-color: black; color:white;">
+                        <div class="card mb-4 mt-4 card-custom p-2">
                             <div class="row g-2">
                                 <div class="col-md-4">
-                                    {{--  <a href="{{ route('detailscreen', $stream['stream_guid']) }}">
-                                    <div class="thumbnail_img">
-                                        <div>
-                                            <img src="{{ $stream['stream_poster'] }}"
-                                                alt="{{ $stream['stream_title'] }}">
-                                        </div>
-                                        <div class="detail_box_hide">
-                                            <div class="detailbox_time">
-                                                {{ $stream['stream_duration_timeformat'] ?? '' }}
-                                            </div>
-                                            <div class="deta_box">
-                                                <div class="season_title">
-                                                </div>
-                                                <div class="content_title"> {{ $stream['stream_title'] }}</div>
-                                                <div class="content_description">
-                                                    {{ $stream['stream_description'] }}
-                                                </div>
-                                                @if ($stream['stream_watched_dur_in_pct'] > 1)
-                                                    <div
-                                                        style="background-color:#555455;height:5px; border-radius:2px;margin-top:10px;">
-                                                        <div
-                                                            style="background-color:#07659E;height:5px;border-radius:2px;width:{{ $stream['stream_watched_dur_in_pct'] }}%">
-                                                        </div>
-                                                    </div>
-                                                @endif
-                                            </div>
-                                        </div>
-                                    </div>
-                                </a>  --}}
                                     <a href="{{ route('detailscreen', $stream['stream_guid']) }}">
                                         <img src="{{ $stream['stream_poster'] }}" alt="{{ $stream['stream_title'] }}"
                                             class="img-fluid" style="position: relative;">
@@ -92,7 +46,7 @@
                                     </a>
                                 </div>
                                 <div class="col-md-8 ">
-                                    <div class="card-body">
+                                    <div class="card-body p-0">
                                         <h5 class="card-title">{{ $stream['stream_title'] }}</h5>
                                         <div class="content-timing">
                                             @if ($stream['released_year'])
