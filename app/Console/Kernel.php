01adbd141ee0afacd6352f854bd537cd1ff73049
sibihaseb <abdul.haseeb.ali@gmail.com>
2024-09-18 16:39:23 +0500

    Removed Api

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,6 +4,7 @@ namespace App\Console;
 
 use App\Services\Api;
 use App\Models\AppCofig;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
@@ -16,16 +17,6 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->call(function () {
-            $response = Http::timeout(300)->withHeaders(Api::headers())
-                ->get(Api::endpoint("/masterfeed"));
-            $appconfig = AppCofig::where('app_code', env('APP_CODE'))->first();
-            if ($appconfig) {
-                $appconfig->update(['api_data' => $response->body()]);
-            } else {
-                AppCofig::create(['app_code' => env('APP_CODE'), 'api_data' => $response->body()]);
-            }
-        })->everyMinute();
     }
 
     /**
