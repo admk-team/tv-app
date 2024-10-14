eb3d2b4f80964f065bc87d1e2fc91dab85daab05
usamaramzan978 <usamaramzan978@email.com>
2024-10-14 16:58:31 +0500

    working on mouse-hover and play trailer

--- resources/views/components/category-slider.blade.php
+++ resources/views/components/category-slider.blade.php
@@ -1,5 +1,8 @@
 @push('style')
     <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/cards-item.css') }}" />
+    {{-- <link href="https://vjs.zencdn.net/8.10.0/video-js.css" rel="stylesheet" /> --}}
+    <link href="https://vjs.zencdn.net/7.15.4/video-js.css" rel="stylesheet">
+    <script src="https://vjs.zencdn.net/7.20.3/video.min.js" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 @endpush
 <section class="sliders">
     <div class="slider-container">
@@ -38,7 +41,7 @@
                             $cartMainCls = 'landscape_slider';
                             $cartMainSubCls = 'ripple';
                             $cardThumbCls = '';
-                            // $cardThumbCls2 = "thumbnail_img thumbnailfourbyfour ST_{$items}";
+                            $cardThumbCls2 = "thumbnail_img thumbnailfourbyfour ST_{$items}";
                             $cardThumbCls2 = "thumbnail_img ST_{$items}";
                             $streamPosterKey = 'stream_sdm';
                             $autoplay = \App\Services\AppConfig::get()->app->colors_assets_for_branding
@@ -56,7 +59,7 @@
                             $cartMainCls = 'billboard_ads';
                             $cartMainSubCls = "ripple BA_{$items}";
                             // $cartMainSubCls = "ripple";
-                           $cardThumbCls = '';
+                            $cardThumbCls = '';
                             $cardThumbCls2 = 'thumbnail_img billboard_img';
                             $streamPosterKey = 'stream_poster';
                             break;
@@ -85,8 +88,8 @@
                             @php
                                 if (
                                     (isset(\App\Services\AppConfig::get()->app->app_info->bypass_detailscreen) &&
-                                    \App\Services\AppConfig::get()->app->app_info->bypass_detailscreen == 1) ||
-                                    ( $stream->bypass_detailscreen == 1)
+                                        \App\Services\AppConfig::get()->app->app_info->bypass_detailscreen == 1) ||
+                                    $stream->bypass_detailscreen == 1
                                 ) {
                                     $screen = 'playerscreen';
                                 } else {
@@ -100,7 +103,7 @@
                                     }
                                 }
                             @endphp
-                            <div class="item">
+                            <div class="item video-link">
                                 <div class="{{ $cartMainSubCls }}">
                                     @if (($category->is_top10 ?? null) === 'Y')
                                         <a href="{{ $url }}">
@@ -151,28 +154,35 @@
                                                             @endif
                                                         @endif
                                                     @endif
-
                                                     <div class="{{ $cardThumbCls }}">
                                                         <img src="{{ $stream->{$streamPosterKey} }}"
                                                             alt="{{ $stream->stream_title }}">
                                                     </div>
-                                                    <div class="detail_box_hide">
-                                                        @if ($stream->stream_duration_timeformat !== '00:00')
-                                                            <div class="detailbox_time">
-                                                                {{ $stream->stream_duration_timeformat ?? '' }}
-                                                            </div>
-                                                        @endif
-                                                        <div class="deta_box">
-                                                            <div class="season_title"></div>
-                                                            <div class="content_title">{{ $stream->stream_title }}
-                                                            </div>
-                                                            @if ($stream->stream_description)
-                                                                <div class="content_description">
-                                                                    {{ $stream->stream_description }}
+                                                    @if ($stream->stream_promo_url !== '' && !in_array($category->card_type, ['BA', 'LB']))
+                                                        <video id="my-video-{{ $stream->stream_guid }}" preload="none"
+                                                            class="card-video-js vjs-tech" muted>
+                                                            <source src="{{ $stream->stream_promo_url }}"
+                                                                type="application/x-mpegURL">
+                                                        </video>
+                                                    @else
+                                                        <div class="detail_box_hide">
+                                                            @if ($stream->stream_duration_timeformat !== '00:00')
+                                                                <div class="detailbox_time">
+                                                                    {{ $stream->stream_duration_timeformat ?? '' }}
                                                                 </div>
                                                             @endif
+                                                            <div class="deta_box">
+                                                                <div class="season_title"></div>
+                                                                <div class="content_title">{{ $stream->stream_title }}
+                                                                </div>
+                                                                @if ($stream->stream_description)
+                                                                    <div class="content_description">
+                                                                        {{ $stream->stream_description }}
+                                                                    </div>
+                                                                @endif
+                                                            </div>
                                                         </div>
-                                                    </div>
+                                                    @endif
                                                 </div>
                                             </div>
                                         </a>
@@ -227,49 +237,38 @@
                                                         @endif
                                                     @endif
                                                 @endif
-
-                                                @if (($stream->is_newly_added ?? 'N') === 'Y')
-                                                    <div class="newly-added-label">
-                                                        <span>New Episode</span>
-                                                    </div>
-                                                @endif
                                                 <div class="{{ $cardThumbCls }} ">
                                                     <img src="{{ $stream->{$streamPosterKey} }}"
                                                         alt="{{ $stream->stream_title }}">
                                                 </div>
-                                                <div class="detail_box_hide">
-                                                    @if ($stream->stream_duration_timeformat !== '00:00')
-                                                        <div class="detailbox_time">
-                                                            {{ $stream->stream_duration_timeformat ?? '' }}
-                                                        </div>
-                                                    @endif
-                                                    <div class="deta_box">
-                                                        <div class="season_title">
-                                                            {{ $stream?->stream_episode_title && $stream?->stream_episode_title !== 'NULL' ? $stream?->stream_episode_title : '' }}
-                                                        </div>
-                                                        <div class="content_title">{{ $stream->stream_title }}</div>
-                                                        @if ($stream->stream_description)
-                                                            <div class="content_description">
-                                                                {{ $stream->stream_description }}
+                                                @if ($stream->stream_promo_url !== '' && !in_array($category->card_type, ['BA', 'LB']))
+                                                    <video id="my-video-{{ $stream->stream_guid }}" preload="none"
+                                                        class="card-video-js vjs-tech" muted>
+                                                        <source src="{{ $stream->stream_promo_url }}"
+                                                            type="application/x-mpegURL">
+                                                    </video>
+                                                @else
+                                                    <div class="detail_box_hide">
+                                                        @if ($stream->stream_duration_timeformat !== '00:00')
+                                                            <div class="detailbox_time">
+                                                                {{ $stream->stream_duration_timeformat ?? '' }}
                                                             </div>
                                                         @endif
-                                                        @if ($stream->stream_watched_dur_in_pct > 1)
-                                                            {{-- <div style="background-color:{{ \App\Services\AppConfig::get()->app->colors_assets_for_branding->tv_cw_unflled_color }};height:5px; border-radius:2px;margin-top:10px;">
-                                                                    <div style="background-color:{{ \App\Services\AppConfig::get()->app->colors_assets_for_branding->tv_cw_flled_color }};height:5px;border-radius:2px;width:{{ $arrStreamsData['stream_watched_dur_in_pct']}}%"></div>
-                                                                </div> --}}
-                                                            <div
-                                                                style="background-color:#555455;height:5px; border-radius:2px;margin-top:10px;">
-                                                                <div
-                                                                    style="background-color:#07659E;height:5px;border-radius:2px;width:{{ $stream->stream_watched_dur_in_pct }}%">
+                                                        <div class="deta_box">
+                                                            <div class="season_title"></div>
+                                                            <div class="content_title">{{ $stream->stream_title }}
+                                                            </div>
+                                                            @if ($stream->stream_description)
+                                                                <div class="content_description">
+                                                                    {{ $stream->stream_description }}
                                                                 </div>
-                                                            </div>
-                                                        @endif
+                                                            @endif
+                                                        </div>
                                                     </div>
-                                                </div>
+                                                @endif
                                             </div>
                                         </a>
                                     @endif
-                                    </a>
                                 </div>
                             </div>
                         @endforeach
@@ -279,3 +278,44 @@
         @endforeach
     </div>
 </section>
+@push('scripts')
+    <script>
+        document.addEventListener('DOMContentLoaded', () => {
+            const videoLinks = document.querySelectorAll('.video-link');
+            console.log('Found video links:', videoLinks.length);
+
+            videoLinks.forEach((link, index) => {
+                const video = link.querySelector('.card-video-js');
+                if (!video) {
+                    // console.error(`Video element not found for link ${index}:`, link);
+                    return;
+                }
+
+                const player = videojs(video.id, {
+                    muted: false,
+                    preload: 'auto',
+                });
+                player.ready(() => {
+                    console.log(`Player ${index} is ready`);
+
+                    link.addEventListener('mouseenter', () => {
+                         player.pause();
+                        player.muted(false);
+                        player.currentTime(0);
+                        player.play().then(() => {
+                            console.log(`Playing video ${index}`);
+                        }).catch((error) => {
+                            console.error(`Error playing video ${index}:`, error);
+                        });
+                    });
+
+                    link.addEventListener('mouseleave', () => {
+                        player.muted(true);
+                        player.pause();
+                        player.currentTime(0);
+                    });
+                });
+            });
+        });
+    </script>
+@endpush
