47374476cbf162b53f26316556d6b9e79be74030
usamaramzan978 <usamaramzan978@email.com>
2024-11-11 15:19:39 +0500

    created seprate file for GTM Tags

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,6 +3,7 @@
 
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     @include('gtm_tags.head')
     @if (Route::is('detailscreen'))
         @yield('meta-tags')
     @else
