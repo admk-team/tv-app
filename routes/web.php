a324dbcfc7677ce0effb4a65c583ef579864daf0
Khurram <m.khurram4669@gmail.com>
2024-12-04 16:32:30 +0500

    2.Notify on upcoming movies (when available - Can see but cannot play (since it’s not yet available) c4

--- routes/web.php
+++ routes/web.php
@@ -26,6 +26,7 @@ use App\Http\Controllers\FollowController;
 use App\Http\Controllers\GiftStreamController;
 use App\Http\Controllers\LanguageController;
 use App\Http\Controllers\NewsLetterController;
+use App\Http\Controllers\NotifyComingSoonStreamController;
 use App\Http\Controllers\PersonController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\QualityController;
@@ -161,3 +162,8 @@ Route::get('/content-bundle/{stream_guid}', [BundleContentController::class, 'in
 Route::post('/purchase', [BundleContentController::class, 'purchase'])->name('bundle.purchase');
 Route::post('/stripe/purchase', [BundleContentController::class, 'createCheckoutSession'])->name('stripe.purchase');
 Route::get('/bundle/success', [BundleContentController::class, 'success'])->name('bundle.success');
+
+Route::get('{slug?}', [HomeController::class, 'index'])->name('home');
+
+Route::get('check/remind/me', [NotifyComingSoonStreamController::class, 'checkRemindStatus'])->name('check.remind.me');
+Route::post('remind/me', [NotifyComingSoonStreamController::class, 'toggleRemind'])->name('remind.me');
