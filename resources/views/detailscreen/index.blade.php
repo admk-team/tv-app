f77daa4f5b71f509f489d5ae25e115080b1f4387
Khurram <m.khurram4669@gmail.com>
2024-12-04 16:52:39 +0500

    2.Notify on upcoming movies (when available - Can see but cannot play (since it’s not yet available)

--- resources/views/detailscreen/index.blade.php
+++ resources/views/detailscreen/index.blade.php
@@ -421,16 +421,23 @@
                                     </a>
                                 @endif
                             @elseif ($stream_details['notify_label'] == 'coming soon')
-                                <form id="remind-form-desktop" method="POST" action="{{ route('remind.me') }}">
-                                    @csrf
-                                    <input type="hidden" name="stream_code" id="stream-code"
-                                        value="{{ $stream_details['stream_guid'] }}">
-                                    <button class="app-primary-btn rounded" id="remind-button-desktop">
-                                        <i id="desktop-remind-icon" class="fas fa-bell"></i>
-                                        <span id="desktop-remind-text">Remind me</span>
-                                    </button>
-                                    <div id="response-message">{{ session('status') }}</div>
-                                </form>
+                                @if (session()->has('USER_DETAILS') && session('USER_DETAILS') !== null)
+                                    <form id="remind-form-desktop" method="POST" action="{{ route('remind.me') }}">
+                                        @csrf
+                                        <input type="hidden" name="stream_code" id="stream-code"
+                                            value="{{ $stream_details['stream_guid'] }}">
+                                        <button class="app-primary-btn rounded" id="remind-button-desktop">
+                                            <i id="desktop-remind-icon" class="fas fa-bell"></i>
+                                            <span id="desktop-remind-text">Remind me</span>
+                                        </button>
+                                        <div id="response-message">{{ session('status') }}</div>
+                                    </form>
+                                @else
+                                    <a class="app-primary-btn rounded">
+                                        <i class="fa fa-play"></i>
+                                        Coming Soon
+                                    </a>
+                                @endif
                             @else
                                 <a href="{{ route('playerscreen', $stream_details['stream_guid']) }}"
                                     class="app-primary-btn rounded">
