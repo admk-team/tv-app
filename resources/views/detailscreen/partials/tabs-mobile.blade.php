449e2b10f439d16c6a7f9fd12bc948e8e816683c
Khurram <m.khurram4669@gmail.com>
2025-02-20 17:15:17 +0500

    some changes for reviews type and status

--- resources/views/detailscreen/partials/tabs-mobile.blade.php
+++ resources/views/detailscreen/partials/tabs-mobile.blade.php
@@ -172,9 +172,7 @@ if (session('USER_DETAILS.USER_CODE')) {
             @endif
             <!--End of season section-->
             @if (
-                (isset($stream_details['video_rating']) && $stream_details['video_rating'] === 'E') ||
-                    (isset(\App\Services\AppConfig::get()->app->app_info->global_rating_enable) &&
-                        \App\Services\AppConfig::get()->app->app_info->global_rating_enable == 1))
+                (isset($stream_details['video_rating']) && $stream_details['video_rating'] == 1))
                 <div class="tab" data-tab="reviews"><span>Reviews</span></div>
             @endif
         </div>
@@ -227,7 +225,7 @@ if (session('USER_DETAILS.USER_CODE')) {
                     @if ($ratingsCount > 0)
                         @if (isset($stream_details['rating_type'], $stream_details['video_rating']) &&
                                 $stream_details['rating_type'] === 'stars' &&
-                                $stream_details['video_rating'] === 'E')
+                               $stream_details['video_rating'] == 1)
                             <span class="content_screen themePrimaryTxtColr">
                                 <div class="star active" style="display: inline-flex;">
                                     <svg fill="#ffffff" width="10px" height="10px" viewBox="0 0 32 32"
@@ -247,7 +245,7 @@ if (session('USER_DETAILS.USER_CODE')) {
                             </span>
                         @elseif(isset($stream_details['rating_type'], $stream_details['video_rating']) &&
                                 $stream_details['rating_type'] === 'hearts' &&
-                                $stream_details['video_rating'] === 'E')
+                               $stream_details['video_rating'] == 1)
                             <span class="content_screen themePrimaryTxtColr">
                                 <div class="star active" style="display: inline-flex;">
                                     <svg fill="#ffffff" width="10px" height="10px" viewBox="0 0 32 32"
@@ -267,7 +265,7 @@ if (session('USER_DETAILS.USER_CODE')) {
                             </span>
                         @elseif(isset($stream_details['rating_type'], $stream_details['video_rating']) &&
                                 $stream_details['rating_type'] === 'thumbs' &&
-                                $stream_details['video_rating'] === 'E')
+                                $stream_details['video_rating'] == 1)
                             <span class="content_screen themePrimaryTxtColr">
                                 <div class="star active" style="display: inline-flex; rotate: 180deg">
                                     <svg fill="#6e6e6e" width="10px" height="10px" version="1.1" id="Capa_1"
@@ -282,50 +280,6 @@ if (session('USER_DETAILS.USER_CODE')) {
                                                     d="M54.715,24.957c-0.544,0.357-1.162,0.598-1.806,0.696l-28.871,4.403c-2.228,0.341-3.956,2.257-3.956,4.511v79.825 c0,1.204,33.353,20.624,43.171,30.142c12.427,12.053,21.31,34.681,33.983,54.373c4.405,6.845,10.201,9.759,15.584,9.759 c10.103,0,18.831-10.273,14.493-24.104c-4.018-12.804-8.195-24.237-13.934-34.529c-4.672-8.376,1.399-18.7,10.989-18.7h48.991 c18.852,0,18.321-26.312,8.552-34.01c-1.676-1.32-2.182-3.682-1.175-5.563c3.519-6.572,2.86-20.571-6.054-25.363 c-2.15-1.156-3.165-3.74-2.108-5.941c3.784-7.878,3.233-24.126-8.71-27.307c-2.242-0.598-3.699-2.703-3.405-5.006 c0.909-7.13-0.509-20.86-22.856-26.447C133.112,0.573,128.281,0,123.136,0C104.047,0.001,80.683,7.903,54.715,24.957z">
                                                 </path>
                                             </g>
-                                        </g>
-                                    </svg>
-                                </div>
-                                {{ $ratingsCount ?? 0 }}
-                            </span>
-                        @elseif (isset(
-                                \App\Services\AppConfig::get()->app->app_info->global_rating_enable,
-                                \App\Services\AppConfig::get()->app->app_info->global_rating_type) &&
-                                \App\Services\AppConfig::get()->app->app_info->global_rating_enable == 1 &&
-                                \App\Services\AppConfig::get()->app->app_info->global_rating_type === 'stars')
-                            <span class="content_screen themePrimaryTxtColr">
-                                <div class="star active" style="display: inline-flex;">
-                                    <svg fill="#ffffff" width="10px" height="10px" viewBox="0 0 32 32"
-                                        version="1.1" xmlns="http://www.w3.org/2000/svg" stroke="#ffffff">
-                                        <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
-                                        <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round">
-                                        </g>
-                                        <g id="SVGRepo_iconCarrier">
-                                            <title>star</title>
-                                            <path
-                                                d="M3.488 13.184l6.272 6.112-1.472 8.608 7.712-4.064 7.712 4.064-1.472-8.608 6.272-6.112-8.64-1.248-3.872-7.808-3.872 7.808z">
-                                            </path>
-                                        </g>
-                                    </svg>
-                                </div>
-                                {{ $ratingsCount ?? 0 }}
-                            </span>
-                        @elseif (isset(
-                                \App\Services\AppConfig::get()->app->app_info->global_rating_enable,
-                                \App\Services\AppConfig::get()->app->app_info->global_rating_type) &&
-                                \App\Services\AppConfig::get()->app->app_info->global_rating_enable == 1 &&
-                                \App\Services\AppConfig::get()->app->app_info->global_rating_type === 'hearts')
-                            <span class="content_screen themePrimaryTxtColr">
-                                <div class="star active" style="display: inline-flex;">
-                                    <svg fill="#ffffff" width="10px" height="10px" viewBox="0 0 32 32"
-                                        version="1.1" xmlns="http://www.w3.org/2000/svg" stroke="#545454">
-                                        <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
-                                        <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round">
-                                        </g>
-                                        <g id="SVGRepo_iconCarrier">
-                                            <title>heart</title>
-                                            <path
-                                                d="M0.256 12.16q0.544 2.080 2.080 3.616l13.664 14.144 13.664-14.144q1.536-1.536 2.080-3.616t0-4.128-2.080-3.584-3.584-2.080-4.16 0-3.584 2.080l-2.336 2.816-2.336-2.816q-1.536-1.536-3.584-2.080t-4.128 0-3.616 2.080-2.080 3.584 0 4.128z">
-                                            </path>
                                         </g>
                                     </svg>
                                 </div>
@@ -601,12 +555,11 @@ if (session('USER_DETAILS.USER_CODE')) {
             @endif
         </div>
         @if (
-            (isset($stream_details['video_rating']) && $stream_details['video_rating'] === 'E') ||
-                (isset(\App\Services\AppConfig::get()->app->app_info->global_rating_enable) &&
-                    \App\Services\AppConfig::get()->app->app_info->global_rating_enable == 1))
+            (isset($stream_details['video_rating']) && $stream_details['video_rating'] == 1))
             <div data-tab-content="reviews" class="content d-none"><!--Start of Ratings section-->
                 <div class="item-ratings">
                     <h1 class="section-title" style="display: flex; align-items: center; gap: 10px;">
+                        Reviews:
                         <div id="rating-icon">
                             @include('detailscreen.partials.rating-icon', [
                                 'ratingsCount' => $ratingsCount,
@@ -649,7 +602,7 @@ if (session('USER_DETAILS.USER_CODE')) {
                         {{-- Stars  --}}
                         @if (isset($stream_details['rating_type'], $stream_details['video_rating']) &&
                                 $stream_details['rating_type'] === 'stars' &&
-                                $stream_details['video_rating'] === 'E')
+                                $stream_details['video_rating'] == 1)
                             <div class="review-rating user-rating-mobile">
                                 @for ($i = 1; $i <= 5; $i++)
                                     <div class="star-mobile" data-rating-mobile="{{ $i }}"
@@ -672,7 +625,7 @@ if (session('USER_DETAILS.USER_CODE')) {
                             </div>
                         @elseif(isset($stream_details['rating_type'], $stream_details['video_rating']) &&
                                 $stream_details['rating_type'] === 'hearts' &&
-                                $stream_details['video_rating'] === 'E')
+                                $stream_details['video_rating'] == 1)
                             {{-- Hearts  --}}
                             <div class="review-rating user-rating-mobile">
                                 @for ($i = 1; $i <= 5; $i++)
@@ -696,7 +649,7 @@ if (session('USER_DETAILS.USER_CODE')) {
                             </div>
                         @elseif(isset($stream_details['rating_type'], $stream_details['video_rating']) &&
                                 $stream_details['rating_type'] === 'thumbs' &&
-                                $stream_details['video_rating'] === 'E')
+                                $stream_details['video_rating'] == 1)
                             {{-- Thumbs  --}}
                             <div class="review-rating user-rating-mobile">
                                 @for ($i = 1; $i <= 5; $i++)
@@ -716,57 +669,6 @@ if (session('USER_DETAILS.USER_CODE')) {
                                                         d="M54.715,24.957c-0.544,0.357-1.162,0.598-1.806,0.696l-28.871,4.403c-2.228,0.341-3.956,2.257-3.956,4.511v79.825 c0,1.204,33.353,20.624,43.171,30.142c12.427,12.053,21.31,34.681,33.983,54.373c4.405,6.845,10.201,9.759,15.584,9.759 c10.103,0,18.831-10.273,14.493-24.104c-4.018-12.804-8.195-24.237-13.934-34.529c-4.672-8.376,1.399-18.7,10.989-18.7h48.991 c18.852,0,18.321-26.312,8.552-34.01c-1.676-1.32-2.182-3.682-1.175-5.563c3.519-6.572,2.86-20.571-6.054-25.363 c-2.15-1.156-3.165-3.74-2.108-5.941c3.784-7.878,3.233-24.126-8.71-27.307c-2.242-0.598-3.699-2.703-3.405-5.006 c0.909-7.13-0.509-20.86-22.856-26.447C133.112,0.573,128.281,0,123.136,0C104.047,0.001,80.683,7.903,54.715,24.957z">
                                                     </path>
                                                 </g>
-                                            </g>
-                                        </svg>
-                                    </div>
-                                @endfor
-                            </div>
-                        @elseif (isset(
-                                \App\Services\AppConfig::get()->app->app_info->global_rating_enable,
-                                \App\Services\AppConfig::get()->app->app_info->global_rating_type) &&
-                                \App\Services\AppConfig::get()->app->app_info->global_rating_enable == 1 &&
-                                \App\Services\AppConfig::get()->app->app_info->global_rating_type === 'stars')
-                            <div class="review-rating user-rating-mobile">
-                                @for ($i = 1; $i <= 5; $i++)
-                                    <div class="star-mobile" data-rating-mobile="{{ $i }}"
-                                        onclick="handleStarRatingMobile(this)">
-                                        <svg fill="#ffffff" width="27px" height="27px" viewBox="0 0 32 32"
-                                            version="1.1" xmlns="http://www.w3.org/2000/svg" stroke="#ffffff">
-                                            <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
-                                            <g id="SVGRepo_tracerCarrier" stroke-linecap="round"
-                                                stroke-linejoin="round">
-                                            </g>
-                                            <g id="SVGRepo_iconCarrier">
-                                                <title>star</title>
-                                                <path
-                                                    d="M3.488 13.184l6.272 6.112-1.472 8.608 7.712-4.064 7.712 4.064-1.472-8.608 6.272-6.112-8.64-1.248-3.872-7.808-3.872 7.808z">
-                                                </path>
-                                            </g>
-                                        </svg>
-                                    </div>
-                                @endfor
-                            </div>
-                        @elseif (isset(
-                                \App\Services\AppConfig::get()->app->app_info->global_rating_enable,
-                                \App\Services\AppConfig::get()->app->app_info->global_rating_type) &&
-                                \App\Services\AppConfig::get()->app->app_info->global_rating_enable == 1 &&
-                                \App\Services\AppConfig::get()->app->app_info->global_rating_type === 'hearts')
-                            {{-- Hearts  --}}
-                            <div class="review-rating user-rating-mobile">
-                                @for ($i = 1; $i <= 5; $i++)
-                                    <div class="star-mobile" data-rating-mobile="{{ $i }}"
-                                        onclick="handleStarRatingMobile(this)">
-                                        <svg fill="#ffffff" width="27px" height="27px" viewBox="0 0 32 32"
-                                            version="1.1" xmlns="http://www.w3.org/2000/svg" stroke="#545454">
-                                            <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
-                                            <g id="SVGRepo_tracerCarrier" stroke-linecap="round"
-                                                stroke-linejoin="round">
-                                            </g>
-                                            <g id="SVGRepo_iconCarrier">
-                                                <title>heart</title>
-                                                <path
-                                                    d="M0.256 12.16q0.544 2.080 2.080 3.616l13.664 14.144 13.664-14.144q1.536-1.536 2.080-3.616t0-4.128-2.080-3.584-3.584-2.080-4.16 0-3.584 2.080l-2.336 2.816-2.336-2.816q-1.536-1.536-3.584-2.080t-4.128 0-3.616 2.080-2.080 3.584 0 4.128z">
-                                                </path>
                                             </g>
                                         </svg>
                                     </div>
