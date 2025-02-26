81dab45723724d1d12ff92c20fbf241249c94b2a
Khurram <m.khurram4669@gmail.com>
2025-02-26 15:58:19 +0500

    minor change for message

--- resources/views/page/movienotexist.blade.php
+++ resources/views/page/movienotexist.blade.php
@@ -1,10 +1,11 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container-fluid" style="margin: 150px 0px 150px 0px;">
-        <div class="row">
-            <h4 style="alfont-weight:bold; text-align: center;
-            color: antiquewhite;"> Sorry, this content is not
-                available in your country</h4>
+    <div class="container text-center">
+        <div class="row align-items-center justify-content-center" style="height: 45vh; ">
+            <div class="col-xl-7">
+                <p class="fs-3 fw-semibold mb-3 theme-active-color">Sorry, this content is not available in your country</p>
+            </div>
         </div>
-    @endsection
+    </div>
+@endsection
