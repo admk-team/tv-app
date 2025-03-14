6f71cd24c40aa27c4b98cb5e7815f509bd44495a
sibihaseb <abdul.haseeb.ali@gmail.com>
2025-03-14 15:43:45 +0500

    Restriction Added

--- app/Http/Controllers/Api/SiteCloneController.php
+++ app/Http/Controllers/Api/SiteCloneController.php
@@ -20,41 +20,41 @@ class SiteCloneController extends Controller
         $input['name'] = Str::replace(' ', '', $input['name']);
         $input['name'] = Str::substr($input['name'], 0, 7);
 
-        // if ($referer === '46.202.176.117') {
-        $response = Http::withOptions([
-            'verify' => false,
-        ])->withHeaders([
-            'Content-Type' => 'application/json',
-        ])->post('https://147.93.114.167:8090/api/createWebsite', [
-            "adminUser" => "admin",
-            "adminPass" => "2MQVOWTfUdFJLTJw",
-            "domainName" => $input['subdomain'] . '.octv.online',
-            "ownerEmail" => "abdul.haseeb.ali@gmail.com",
-            "packageName" => "Default",
-            "websiteOwner" => "admin",
-            "ownerPassword" => "2MQVOWTfUdFJLTJw"
-        ]);
-        // Get response data
-        $createdWebsite = $response->json(); // Decodes JSON response into an associative array
-        // Define script path
-        Log::info($createdWebsite);
-        $scriptPath = base_path('finalscript.sh');
+        if ($referer === '46.202.176.117') {
+            $response = Http::withOptions([
+                'verify' => false,
+            ])->withHeaders([
+                'Content-Type' => 'application/json',
+            ])->post('https://147.93.114.167:8090/api/createWebsite', [
+                "adminUser" => "admin",
+                "adminPass" => "2MQVOWTfUdFJLTJw",
+                "domainName" => $input['subdomain'] . '.octv.online',
+                "ownerEmail" => "abdul.haseeb.ali@gmail.com",
+                "packageName" => "Default",
+                "websiteOwner" => "admin",
+                "ownerPassword" => "2MQVOWTfUdFJLTJw"
+            ]);
+            // Get response data
+            $createdWebsite = $response->json(); // Decodes JSON response into an associative array
+            // Define script path
+            Log::info($createdWebsite);
+            $scriptPath = base_path('finalscript.sh');
 
-        // Define arguments
-        $arg1 = escapeshellarg("/home/{$input['subdomain']}.octv.online/public_html");
-        $arg2 = escapeshellarg($input['app_code']);
-        $arg3 = escapeshellarg("{$input['subdomain']}.octv.online");
+            // Define arguments
+            $arg1 = escapeshellarg("/home/{$input['subdomain']}.octv.online/public_html");
+            $arg2 = escapeshellarg($input['app_code']);
+            $arg3 = escapeshellarg("{$input['subdomain']}.octv.online");
 
-        // Run script with arguments
-        $command = "echo 'iltbtlmn3520ashT@' | su - root -c 'sleep 120; /home/octv.online/public_html/finalscript.sh $arg1 $arg2 $arg3' 2>&1";
-        $output = shell_exec($command);
+            // Run script with arguments
+            $command = "echo 'iltbtlmn3520ashT@' | su - root -c 'sleep 120; /home/octv.online/public_html/finalscript.sh $arg1 $arg2 $arg3' 2>&1";
+            $output = shell_exec($command);
 
-        return response()->json([
-            'success' => $referer,
-            'script_output' => $output
-        ]);
-        // }
+            return response()->json([
+                'success' => $referer,
+                'script_output' => $output
+            ]);
+        }
 
-        // return response()->json(['error' => 'Try Again Later']);
+        return response()->json(['error' => 'Try Again Later']);
     }
 }
