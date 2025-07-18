04a9f24419950e1d542d3dbb9806bc7f297fe5c9
Khurram <m.khurram4669@gmail.com>
2025-07-18 12:41:47 +0500

    themeSecondaryTxtColor change in advertiser navbar

--- resources/views/advertiser_banner_ads/nav-bar.blade.php
+++ resources/views/advertiser_banner_ads/nav-bar.blade.php
@@ -1,14 +1,14 @@
  <ul class="nav nav-underline justify-content-center">
      <li class="nav-item">
          <a class="nav-link {{ request()->is('advertiser/banner_ad') ? 'active' : '' }}"
-             href="{{ url('/advertiser/banner_ad') }}" style="color: var(--themeSecondaryTxtColor);">Banner Ads</a>
+             href="{{ url('/advertiser/banner_ad') }}" style="color: var(--themePrimaryTxtColor);">Banner Ads</a>
      </li>
      <li class="nav-item">
          <a class="nav-link {{ request()->is('advertiser/overlay_ad') ? 'active' : '' }}"
-             href="{{ url('/advertiser/overlay_ad') }}" style="color: var(--themeSecondaryTxtColor);">Overlay Ads</a>
+             href="{{ url('/advertiser/overlay_ad') }}" style="color: var(--themePrimaryTxtColor);">Overlay Ads</a>
      </li>
      <li class="nav-item">
          <a class="nav-link {{ request()->is('advertiser/video_ad') ? 'active' : '' }}"
-             href="{{ url('/advertiser/video_ad') }}" style="color: var(--themeSecondaryTxtColor);">Video Ads</a>
+             href="{{ url('/advertiser/video_ad') }}" style="color: var(--themePrimaryTxtColor);">Video Ads</a>
      </li>
  </ul>
