1178362e9e05c669a5994e38d1016828af413f2e
Khurram <m.khurram4669@gmail.com>
2025-07-18 12:43:29 +0500

    minor change in navbar advertiser

--- resources/views/advertiser_banner_ads/nav-bar.blade.php
+++ resources/views/advertiser_banner_ads/nav-bar.blade.php
@@ -1,11 +1,11 @@
  <ul class="nav nav-underline justify-content-center">
      <li class="nav-item">
-         <a class="nav-link {{ request()->is('advertiser/banner_ad') ? 'active' : '' }}"
-             href="{{ url('/advertiser/banner_ad') }}" style="color: var(--themePrimaryTxtColor);">Banner Ads</a>
-     </li>
-     <li class="nav-item">
          <a class="nav-link {{ request()->is('advertiser/overlay_ad') ? 'active' : '' }}"
              href="{{ url('/advertiser/overlay_ad') }}" style="color: var(--themePrimaryTxtColor);">Overlay Ads</a>
+     </li>
+     <li class="nav-item">
+         <a class="nav-link {{ request()->is('advertiser/banner_ad') ? 'active' : '' }}"
+             href="{{ url('/advertiser/banner_ad') }}" style="color: var(--themePrimaryTxtColor);">Banner Ads</a>
      </li>
      <li class="nav-item">
          <a class="nav-link {{ request()->is('advertiser/video_ad') ? 'active' : '' }}"
